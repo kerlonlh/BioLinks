525d5979c7dc6b43177fc9c145d6285a1139a47f
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2025-07-26 14:24:00 -0300

    Registrando um usuário

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,18 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegistrerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 
+Route::get('/register', [RegistrerController::class, 'index'])->name('register');
+Route::post('/register', [RegistrerController::class, 'register']);
 
-Route::get('/dashboard', fn()=> 'dashboard ::'.auth()->id())->middleware('auth')->name('dashboard');
+
+Route::get('/dashboard', fn()=> 'dashboard :: '.auth()->id())->middleware('auth')->name('dashboard');
