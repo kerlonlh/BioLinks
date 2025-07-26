883d643b67e70fccbd6bef409f98636c5a5da2e3
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2025-07-26 17:21:34 -0300

    Middlewares Guest e Auth

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegistrerController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
@@ -9,10 +10,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [LoginController::class, 'index'])->name('login');
-Route::post('/login', [LoginController::class, 'login']);
+Route::middleware(['guest'])->group(function () {
 
-Route::get('/register', [RegistrerController::class, 'index'])->name('register');
-Route::post('/register', [RegistrerController::class, 'register']);
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
 
-Route::get('/dashboard', fn()=> 'dashboard :: ' . Auth::user()->id)->middleware('auth')->name('dashboard');
+    Route::get('/register', [RegistrerController::class, 'index'])->name('register');
+    Route::post('/register', [RegistrerController::class, 'register']);
+    
+});
+
+Route::get('/logout', LogoutController::class)->middleware('auth')->name('logout');
+
+Route::get('/dashboard', fn() => 'dashboard :: ' . Auth::user()->id)->middleware('auth')->name('dashboard');
