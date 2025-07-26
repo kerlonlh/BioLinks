adc4a3d11b39e39ae2eddc07e61edfd81f667836
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2025-07-26 14:08:32 -0300

    Refatorando para Form Requests

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Illuminate\Support\Facades\Auth; 
+
 use App\Http\Controllers\Controller;
+use App\Http\Requests\MakeLoginRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -14,25 +15,11 @@ class LoginController extends Controller
         return view('auth.login');
     }
 
-    public function login(){
+    public function login(MakeLoginRequest $request){
 
-        request()->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
-        
-        if($user = User::query()
-            ->where('email', '=', request()->email)
-            ->first()){
-
-                if( Hash::check(request()->password, $user->password)){
-                //auth()->login($user);
-                Auth::login($user);
-
-                return to_route('dashboard');
-                }
-
-            }
+        if ($request->attempt()){
+            return to_route('dashboard');
+        }
         
         return back()->with(['message' => 'Não encontrado!']);
         
