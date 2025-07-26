df88b8d7f011f13a740a4216ebbcaa70532eaa96
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2025-07-26 11:51:47 -0300

    Validação de Formulário

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Support\Facades\Auth; 
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -14,19 +15,26 @@ class LoginController extends Controller
     }
 
     public function login(){
+
+        request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+        
         if($user = User::query()
             ->where('email', '=', request()->email)
             ->first()){
 
-                if( Hash::check(request()->password, $user->password)){;
-                auth()->login($user);
+                if( Hash::check(request()->password, $user->password)){
+                //auth()->login($user);
+                Auth::login($user);
 
                 return to_route('dashboard');
                 }
 
             }
         
-        return back()->with(['messagem' => 'Não encontrado!']);
+        return back()->with(['message' => 'Não encontrado!']);
         
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,31 @@
 <div>
     <h1>Login</h1>
+
+
+    @if ($message = session()->get('message'))
+        <div>
+            {{ $message }}
+        </div>
+    @endif
     <form action="/login" method="POST">
 
         @csrf
 
-        <input type="email" name="email" placeholder="Email" />
-        <input type="password" name="password" placeholder="Senha" />
+        <div>
+            <input name="email" placeholder="Email" value="{{ old('email') }}" />
+
+            @error('email')
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <div>
+            <input type="password" name="password" placeholder="Senha" />
+            @error('password')
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+
 
         <button>Logar</button>
     </form>
