847ef55ca2a07697edf15e14d04747ca85e0bc4b
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2025-08-02 13:05:48 -0300

    Ajuste no DaisyUI

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,9 @@
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
 
-// postcss.config.js
 export default {
-  plugins: {
-    // Aqui você adiciona o plugin do Tailwind CSS para PostCSS
-    '@tailwindcss/postcss': {},
-    // Outros plugins, como o autoprefixer
-    autoprefixer: {},
-  },
-};
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
+}
