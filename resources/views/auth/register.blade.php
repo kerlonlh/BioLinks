a540ec5024da7eac2ba0c7df3d7fe2906f5bcc79
Kerlon Hinterholz <kerlon.l.h@gmail.com>
2025-08-04 22:10:41 -0300

    Refatorando register com blade components

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,48 +1,16 @@
 <x-layout.app>
-    <div>
-
-        {{ auth()->id() }}
-        <h1>Register</h1>
-
-
-        @if ($message = session()->get('message'))
-            <div>
-                {{ $message }}
-            </div>
-        @endif
-        <form action="{{ route('register') }}" method="POST">
-
-            @csrf
-
-            <div>
-                <input name="name" placeholder="Name" value="{{ old('name') }}" />
-
-                @error('name')
-                    <span>{{ $message }}</span>
-                @enderror
-            </div>
-
-            <div>
-                <input name="email" placeholder="Email" value="{{ old('email') }}" />
-
-                @error('email')
-                    <span>{{ $message }}</span>
-                @enderror
-            </div>
-
-            <div>
-                <input name="email_confirmation" placeholder="Email Confirmation" />
-            </div>
-
-            <div>
-                <input type="password" name="password" placeholder="Senha" />
-                @error('password')
-                    <span>{{ $message }}</span>
-                @enderror
-            </div>
-
-
-            <button>Registrar</button>
-        </form>
-    </div>
+    <x-container>
+        <x-card title="Registrar">
+            <x-form :route="route('register')" post id="register-form">
+                <x-input name="name" placeholder="Nome" value="{{ old('name') }}" />
+                <x-input name="email" placeholder="E-mail" value="{{ old('email') }}" />
+                <x-input name="email_confirmation" placeholder="E-mail de confirmação"
+                    value="{{ old('email_confirmation') }}" />
+                <x-input name="password" type="password" placeholder="Senha" />
+            </x-form>
+            <x-slot:actions>
+                <x-button type="submit" form="register-form">Registrar</x-button>
+            </x-slot:actions>
+        </x-card>
+    </x-container>
 </x-layout.app>
